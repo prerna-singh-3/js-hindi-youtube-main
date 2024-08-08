8df7c565ed024f2700c72b389187917c6c2afeb6
Prerna <prernasingh.xa@gmail.com>
2024-08-08 09:41:51 +0530

    dataTypes

--- js-hindi-youtube-main/01_basics/02_dataTypes.js
+++ js-hindi-youtube-main/01_basics/02_dataTypes.js
@@ -5,3 +5,22 @@
 console.log(3 + 3) // code readability should be high
 
 console.log("Prerna")
+
+let name = "prerna"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value has not been assigned yet
+// sumbol => uniqueness
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
