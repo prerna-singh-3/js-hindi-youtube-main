9f3286cbadde78c67719550a86865b1ad2951054
Prerna <prernasingh.xa@gmail.com>
2024-08-09 17:47:28 +0530

    conversion of datatypes

--- js-hindi-youtube-main/01_basics/03_conversionOperation.js
+++ js-hindi-youtube-main/01_basics/03_conversionOperation.js
@@ -29,3 +29,43 @@ let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+// **********Operations**********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " prerna"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+// console.log(+""); // not prefered 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
