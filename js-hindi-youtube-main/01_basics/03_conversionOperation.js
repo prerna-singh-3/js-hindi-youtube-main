6b5e3f381657f4b044f942bc690168e67bedc97d
Prerna <prernasingh.xa@gmail.com>
2024-08-09 13:13:51 +0530

    conversion of datatypes

---
+++ js-hindi-youtube-main/01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "prerna"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); 
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "prerna"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "prerna" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
