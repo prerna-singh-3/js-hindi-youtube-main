d48a8e2a9a68e9e8912fb8f3a9ee84859298f8e1
Prerna <prernasingh.xa@gmail.com>
2024-08-10 16:05:35 +0530

    new

---
+++ datatypes-summary.js
@@ -0,0 +1,23 @@
+// **********NOTES********** //
+
+
+// 1) Primitive Data Types (call by value)
+
+// 7 categories : String, Number, Boolean, Null, Undefined,    Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// 2) Reference Data Type/Non-primitive Data Type (Memory mein refernce directly allocate kiya jata hai)
+
+// Array, Objects, Functions
--- js-hindi-youtube-main/README.md
+++
@@ -1,2 +0,0 @@
-# js-hindi-youtube
-A code repo for javascript series at Chai aur code youtube channel
