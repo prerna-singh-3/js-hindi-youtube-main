6be6d9ee47bece83928582a153b258e1f1cbe951
Prerna <prernasingh.xa@gmail.com>
2024-08-12 21:22:06 +0530

    objects and json

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,77 @@
+// const tinderUser = new Object() // Singleton Object
+
+const tinderUser = {} // Non Singleton Object
+
+tinderUser.id = "123abc" 
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+//  console.log(tinderUser);
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname:{
+        usersfullname:{
+            firstname: "prerna",
+            lastname: "singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.usersfullname);
+// console.log(regularUser.fullname.usersfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign(obj1, obj2)
+// const obj4 = Object.assign({}, obj1, obj2) // Same result as the above syntax but this is more apt and it is more preferd!!!
+
+const obj3 = {...obj1, ...obj2} //90% time we will use this
+// console.log(obj3);
+// console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh",
+}
+//  course.courseInstructor // we can use this there's no problem in it
+
+const {courseInstructor} = course
+// console.log(courseInstructor);
+
+// or
+
+const {courseInstructor: instructor} = course
+// console.log(instructor);
+
